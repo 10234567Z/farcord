62a7a55a382e97b96f13b487c16ae2a7388b809a
10234567Z <93607971+10234567Z@users.noreply.github.com>
2025-07-15 07:19:37 +0530

    Initialize Workflow

---
+++ src/AccessVerifier.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AccessVerifier {
+
+    // Balance checking for the requirement
+
+    // NFT Ownership verification
+
+    // Temporary Pass Based Access to some servers/channels (kind of like special offer)
+
+    // In case of channel having multi token requirement (like NFT and Token balance both need to be a certain amount rather than being either)
+}
---
+++ src/TokenGateManager.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract TokenGateManager {
+    
+    // New community
+        // --> Set owner/nft owner/min balance requirement
+    
+    // New Channel
+        // --> Set owner/nft owner/min balance requirement
+
+    // Requesting to join/ verifies the requirement
+
+    // Kicking someone out
+
+    // Change the requirement
+}
